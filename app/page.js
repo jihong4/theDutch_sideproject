920279d971b6557abad024637ec4f17717752048
jihong lee <jihong9412@gachon.ac.kr>
2024-05-19 16:19:50 +0900

    logo&start button 1.0.0

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
-export default function Home(){
-  return(
-    <div>
-      <h1>hello</h1>
+import React from 'react';
+import styles from './styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <img src="/images/logo.png" alt="Logo" className={styles.logo} />
+      </div>
+      <button className={styles.startButton}>START</button>
     </div>
   );
-}
+};
+
+export default Home;
